d7f6dd2c9b22dc5d40cc3fedf905e53cffb65db4
METADIDOMIOFFICIEL <161984827+METADIDOMIOFFICIEL@users.noreply.github.com>
2026-01-31 17:01:05 +0100

    Create build.gradle.kts

---
+++ MonTemplateApp/build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    id("com.android.application") version "8.7.2" apply false   // ou 9.0.0 si déjà dispo
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false   // 2.0.20 ou 2.1.0+
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false
+}
